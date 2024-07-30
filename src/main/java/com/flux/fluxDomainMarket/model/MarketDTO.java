bc5e1bd7d70569bda10d16d1674d088fdddedba8
Stjoo0925 <stjoo0925@gmail.com>
2024-07-30 18:02:04 +0900

    feat : update market read logic

---
+++ src/main/java/com/flux/fluxDomainMarket/model/MarketDTO.java
@@ -0,0 +1,32 @@
+package com.flux.fluxDomainMarket.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class MarketDTO {
+
+    private Integer marketId;
+    private String marketName;
+    private String marketImgs;
+    private int marketPrice;
+    private int marketMaxprice;
+    private String marketCategory;
+    private String marketContents;
+    private boolean marketOrderablestatus;
+    private LocalDateTime marketCreateAt;
+    private LocalDateTime marketUpdateAt;
+    private LocalDateTime marketSelldate;
+    private LocalDateTime startDate;
+    private LocalDateTime endDate;
+    private String marketView;
+
+    private Integer userId;
+}
