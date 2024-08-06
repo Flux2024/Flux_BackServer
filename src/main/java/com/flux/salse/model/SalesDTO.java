e6d4fcde678a58fd206d214caae6d9cf15172b3e
Stjoo0925 <stjoo0925@gmail.com>
2024-08-06 14:37:48 +0900

    remove : sales folder delete

--- src/main/java/com/flux/salse/model/SalesDTO.java
+++
@@ -1,4 +0,0 @@
-package com.flux.salse.model;
-
-public class SalesDTO {
-}
--- src/main/java/com/flux/salse/service/SalesService.java
+++
@@ -1,7 +0,0 @@
-package com.flux.salse.service;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public class SalesService {
-}
