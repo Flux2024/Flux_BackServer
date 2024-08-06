4d1965482ba840c8f35460401332fb3cf3c579c5
Jin-tonix <jinheemok815@gmail.com>
2024-08-06 21:22:21 +0900

    doc:notice

--- src/main/java/com/flux/notice/model/NoticeDTO.java
+++ src/main/java/com/flux/notice/model/NoticeDTO.java
@@ -1,16 +1,82 @@
 package com.flux.notice.model;
 
-import lombok.Data;
+import com.fasterxml.jackson.annotation.JsonFormat;
 
 import java.time.LocalDateTime;
 
-@Data
 public class NoticeDTO {
 
-    private Integer noticeId;
-    private String noticeContents;
+    private Long userId;
+
+    private String noticeTitle;
+
+    private String noticeContent;
+
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
     private LocalDateTime noticeCreateAt;
+
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
     private LocalDateTime noticeUpdateAt;
 
-    private Integer userId;
+    public NoticeDTO() {
+    }
+
+    public NoticeDTO(Long userId, String noticeTitle, String noticeContent, LocalDateTime noticeCreateAt, LocalDateTime noticeUpdateAt) {
+        this.userId = userId;
+        this.noticeTitle = noticeTitle;
+        this.noticeContent = noticeContent;
+        this.noticeCreateAt = noticeCreateAt;
+        this.noticeUpdateAt = noticeUpdateAt;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public String getNoticeTitle() {
+        return noticeTitle;
+    }
+
+    public void setNoticeTitle(String noticeTitle) {
+        this.noticeTitle = noticeTitle;
+    }
+
+    public String getNoticeContent() {
+        return noticeContent;
+    }
+
+    public void setNoticeContent(String noticeContent) {
+        this.noticeContent = noticeContent;
+    }
+
+    public LocalDateTime getNoticeCreateAt() {
+        return noticeCreateAt;
+    }
+
+    public void setNoticeCreateAt(LocalDateTime noticeCreateAt) {
+        this.noticeCreateAt = noticeCreateAt;
+    }
+
+    public LocalDateTime getNoticeUpdateAt() {
+        return noticeUpdateAt;
+    }
+
+    public void setNoticeUpdateAt(LocalDateTime noticeUpdateAt) {
+        this.noticeUpdateAt = noticeUpdateAt;
+    }
+
+    @Override
+    public String toString() {
+        return "NoticeDTO{" +
+                "userId=" + userId +
+                ", noticeTitle='" + noticeTitle + '\'' +
+                ", noticeContent='" + noticeContent + '\'' +
+                ", noticeCreateAt=" + noticeCreateAt +
+                ", noticeUpdateAt=" + noticeUpdateAt +
+                '}';
+    }
 }
