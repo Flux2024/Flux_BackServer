9091e0d256546ed389e31e37b951304e7b6fa705
ppudding3861 <ppuding3861@gmail.com>
2024-08-11 23:30:16 +0900

    feat : visitor modify

--- src/main/java/com/flux/visitor/model/Visitor.java
+++ src/main/java/com/flux/visitor/model/Visitor.java
@@ -6,36 +6,24 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 
 import java.time.LocalDate;
 
 @Getter
 @Setter
 @Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Visitor {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private LocalDate visitDate;
-    private int count;
-
-    // 기본 생성자
-    public Visitor() {
-    }
-
-    // 모든 필드를 받는 생성자
-    public Visitor(Long id, LocalDate visitDate, int count) {
-        this.id = id;
-        this.visitDate = visitDate;
-        this.count = count;
-    }
-
-    // 필드 중 id를 제외한 생성자
-    public Visitor(LocalDate visitDate, int count) {
-        this.visitDate = visitDate;
-        this.count = count;
-    }
-
-
+    private Long visitCount;
 }
